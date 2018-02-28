97ffa0b8715592fe1a4d0b30db6913cce1049452
Matthew Schwartz <schwartz_matthew2@bah.com>
2018-02-28 17:13:50 -0500

    Fixed bug where passing empty value to optional argument -d would fail

--- src/main/java/consumerexample/app/ConsumerExample.java
+++ src/main/java/consumerexample/app/ConsumerExample.java
@@ -1,5 +1,27 @@
 package consumerexample.app;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.nio.ByteBuffer;
+import java.nio.charset.Charset;
+import java.util.Arrays;
+import java.util.Properties;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.log4j.Logger;
+
 import com.amazonaws.AmazonClientException;
 import com.amazonaws.AmazonServiceException;
 import com.amazonaws.auth.AWSCredentials;
@@ -15,21 +37,9 @@ import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
-import org.apache.commons.cli.*;
-import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.apache.kafka.clients.consumer.ConsumerRecords;
-import org.apache.kafka.clients.consumer.KafkaConsumer;
-import org.apache.log4j.Logger;
+
 import us.dot.its.jpo.ode.plugin.j2735.J2735Bsm;
 import us.dot.its.jpo.ode.util.SerializationUtils;
-
-import java.io.*;
-import java.nio.ByteBuffer;
-import java.nio.charset.Charset;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.Arrays;
-import java.util.Properties;
 public class ConsumerExample {
 	private static final Logger log = Logger.getLogger(ConsumerRecord.class);
 
@@ -41,6 +51,7 @@ public class ConsumerExample {
 		
 		Option destination_option = new Option("d", "destination", true, "Optional, destination defaults to Firehose. Enter \"s3\" to override");
 		destination_option.setRequired(false);
+		destination_option.setOptionalArg(true);
 		options.addOption(destination_option);
 		
 		Option bucket_name_option = new Option("s", "bucket-name", true, "Bucket Name");
