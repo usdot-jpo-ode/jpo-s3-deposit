f38c3cbf1a0d386c33a1095faa241f3004ed16b0
Daniel Stephenson <dstephenson@trihydrol.com>
2022-02-18 19:29:51 +0000

    Modeled the simple run script off of the example usage in the README.

--- scripts/simple-run.sh
+++ scripts/simple-run.sh
@@ -1,1 +1,3 @@
-java -jar ./target/jpo-aws-depositor-jar-with-dependencies.jar -b ${DEPOSIT_BUCKET_NAME} -k ${DEPOSIT_KEY_NAME} -s ${DOCKER_HOST_IP}:9092 -t ${DEPOSIT_TOPIC} -g ${DEPOSIT_GROUP}
+echo "Executing."
+java -jar ./target/jpo-aws-depositor-jar-with-dependencies.jar --bootstrap-server ${DOCKER_HOST_IP}:9092 -g ${DEPOSIT_GROUP} -t ${DEPOSIT_TOPIC} -b ${DEPOSIT_BUCKET_NAME} -k ${DEPOSIT_KEY_NAME}
+echo "Finished executing."
