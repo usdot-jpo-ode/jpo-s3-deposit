bd75c6d39115039f32ca96a9c088bb5202a1d313
Daniel Stephenson <dstephenson@trihydrol.com>
2022-02-23 18:29:33 +0000

    Added a launch configuration.

--- scripts/simple-run.sh
+++ scripts/simple-run.sh
@@ -1,3 +1,3 @@
 echo "Executing."
-java -jar ./target/jpo-aws-depositor-jar-with-dependencies.jar --bootstrap-server ${DOCKER_HOST_IP}:9092 -g ${DEPOSIT_GROUP} -t ${DEPOSIT_TOPIC} -b ${DEPOSIT_BUCKET_NAME} -k ${DEPOSIT_KEY_NAME}
+java -jar ./target/jpo-aws-depositor-jar-with-dependencies.jar --bootstrap-server ${DOCKER_HOST_IP}:9092 -g ${DEPOSIT_GROUP} -t ${DEPOSIT_TOPIC} -b ${DEPOSIT_BUCKET_NAME} -k ${DEPOSIT_KEY_NAME} -d s3
 echo "Finished executing."
