b64ed04c73899fa4a5813cee40d65bf0518041c8
dmccoystephenson <dmccoystephenson@gmail.com>
2022-03-18 13:56:51 -0600

    Removed some unnecessary convenience scripts.

--- compile.sh
+++
@@ -1,6 +0,0 @@
-# note: must be run from the project root folder
-
-# compile
-echo "Compiling."
-mvn clean package assembly:single
-echo "Finished compiling."
--- simple-run.sh
+++
@@ -1,3 +0,0 @@
-echo "Executing."
-java -Dlogback.configurationFile=./src/main/resources/logback.xml -jar ./target/jpo-aws-depositor-jar-with-dependencies.jar --bootstrap-server ${DOCKER_HOST_IP}:9092 -g ${DEPOSIT_GROUP} -t ${DEPOSIT_TOPIC} -b ${DEPOSIT_BUCKET_NAME} -k ${DEPOSIT_KEY_NAME} -d s3
-echo "Finished executing."
